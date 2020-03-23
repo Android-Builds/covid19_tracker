0a4ebf8529aa1496850e0c2d9e04d186e98416ed
xayron <zxyron0@gmail.com>
2020-03-23 14:35:24 +0530

    Add remove dropped down just to keep rederence [will remove eventually]

--- lib/pages/tabs/global.dart
+++ lib/pages/tabs/global.dart
@@ -17,6 +17,8 @@ class GlobalPage extends StatefulWidget {
 class _GlobalPageState extends State<GlobalPage> {
 
   List<Info> info = new List<Info>();
+  List<String> countries = ['China','India'];
+  String dropDownValue = 'India';
 
   getinfo(String con) {
     // getInfo(con).then((_info) {
@@ -28,10 +30,22 @@ class _GlobalPageState extends State<GlobalPage> {
     // });
   }
 
+  setcountrieslist() {
+    countries = [];
+    for(int i =0; i<widget.info.length; i++) {
+      if(widget.info[i].province != "") {
+        countries.add(widget.info[i].province + ' (' + widget.info[i].country + ')');
+      } else {
+        countries.add(widget.info[i].country);
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     // getlatest();
+    setcountrieslist();
   }
 
   @override
@@ -48,6 +62,34 @@ class _GlobalPageState extends State<GlobalPage> {
                 SizedBox(height: 20.0),
                 Text('Recovered: ' + widget.latest.recovered.toString()),
                 SizedBox(height: 20.0),
+                Center(
+                  child: DropdownButton<String>(
+                    value: dropDownValue,
+                    icon: Icon(Icons.arrow_drop_down),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(
+                      color: Colors.grey
+                    ),
+                    underline: Container(
+                      color: Colors.transparent,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropDownValue = newValue;
+                      });
+                      getinfo(newValue);
+                    },
+                    items: countries
+                    .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Center(child: Text(value)),
+                      );
+                    })
+                    .toList(),
+                  ),
+                ),
               ],
             ),
           ),
